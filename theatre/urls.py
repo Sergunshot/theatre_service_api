267cc5e5175586c67aeec8845502cd32c8e2b5f1
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-10 08:41:47 +0200

    add genres url

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,12 +1,17 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from theatre.views import TheatreHallViewset, ActorViewset
+from theatre.views import (
+    TheatreHallViewset,
+    ActorViewset,
+    GenreViewset
+)
 
 router = routers.DefaultRouter()
 
 router.register("theatre_halls", TheatreHallViewset)
 router.register("actors", ActorViewset)
+router.register("genres", GenreViewset)
 
 urlpatterns = [
     path("", include(router.urls)),
