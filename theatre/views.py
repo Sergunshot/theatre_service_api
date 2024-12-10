7a8b0ff2e95798cf4ecd4832d0bb5e5c97d16ec0
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-10 07:35:22 +0200

    add TheatreHallViewset

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from theatre.models import TheatreHall
+from theatre.serializers import TheatreHallSerializer
+
+
+class TheatreHallViewset(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
