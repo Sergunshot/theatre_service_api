a544e7e6a4f87cb195dea0ccc0fc8e9df2221a9f
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-10 08:17:05 +0200

    add actors url

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from theatre.views import TheatreHallViewset
+from theatre.views import TheatreHallViewset, ActorViewset
 
 router = routers.DefaultRouter()
 
 router.register("theatre_halls", TheatreHallViewset)
+router.register("actors", ActorViewset)
 
 urlpatterns = [
     path("", include(router.urls)),
