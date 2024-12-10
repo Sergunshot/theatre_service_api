3b0ffe3fcd8775d4f98ae11f13c683c05dd80316
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-10 08:38:07 +0200

    add Genre model

--- theatre/models.py
+++ theatre/models.py
@@ -25,3 +25,9 @@ class Actor(models.Model):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
