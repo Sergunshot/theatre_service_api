d8b0efcf7049de96fc959c6278c828411b3c1ca2
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-17 17:56:24 +0200

    register Play model

--- theatre/admin.py
+++ theatre/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import (
     TheatreHall,
     Actor,
-    Genre
+    Genre,
+    Play
 )
 
 admin.site.register(TheatreHall)
 admin.site.register(Actor)
 admin.site.register(Genre)
+admin.site.register(Play)
