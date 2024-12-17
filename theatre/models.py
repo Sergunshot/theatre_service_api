916ace9462864244fb44af4eb0f2ad89991600e0
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-17 17:54:28 +0200

    add Play model

--- theatre/models.py
+++ theatre/models.py
@@ -31,3 +31,18 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
