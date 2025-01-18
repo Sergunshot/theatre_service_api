6f512bfed8317f462f1a39a5f583bed350b5f2bd
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-18 18:34:44 +0200

    customize Ticket model, add staticmethod validate_ticket

--- theatre/models.py
+++ theatre/models.py
@@ -84,5 +84,26 @@ class Ticket(models.Model):
         on_delete=models.CASCADE
     )
 
+    class Meta:
+        unique_together = ("row", "seat", "performance")
+        ordering = ["row", "seat"]
+
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {theatre_hall_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
     def __str__(self):
         return f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
