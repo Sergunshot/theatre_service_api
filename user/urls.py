1b28b71e9b1970f391e5731c21868946c7db977b
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-19 18:05:10 +0200

    add manage_user url

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="get_token")
+    path("login/", CreateTokenView.as_view(), name="get_token"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 
 app_name = "user"
