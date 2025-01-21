51d57c577f0736fcadfbeeea4e6215ee8f1a0f66
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-21 21:06:56 +0200

    add upload_image method

--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from theatre.permissions import IsAdminAllORIsAuthenticatedORReadOnly
 
@@ -67,6 +69,19 @@ class PlayViewset(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return PlayDetailSerializer
         return PlaySerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image"
+    )
+    def upload_image(self, request, pk=None):
+        play = self.get_object()
+        serializer = self.get_serializer(play, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_queryset(self):
         """Retrieve the movies with filters"""
