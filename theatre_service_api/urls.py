bdfe62dc038b96b475ffd746262ac3ba2c218792
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-21 18:29:52 +0200

    add static root

--- theatre_service_api/urls.py
+++ theatre_service_api/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls"), name="theatre"),
     path("api/user/", include("user.urls"), name="user"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
