cf01ce9bb895ed17b793023381936de0706919cc
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-23 12:37:29 +0200

    add unauthenticated user test

---
+++ theatre/tests/tests_theatre_api.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+PLAY_URL = reverse("theatre:play-list")
+
+
+class UnauthenticatedMovieAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(PLAY_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
