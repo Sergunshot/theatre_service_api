ae36b6133bbb8d26688ea405481c6a7677adfaef
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-23 13:33:32 +0200

    add authenticated user test

--- theatre/tests/tests_theatre_api.py
+++ theatre/tests/tests_theatre_api.py
@@ -1,9 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from theatre.models import Play
+from theatre.serializers import PlayListSerializer
+
 PLAY_URL = reverse("theatre:play-list")
+
+
+def sample_play(**params):
+    defaults = {
+        "title": "Sample play",
+        "description": "Sample description",
+        "duration": 90,
+    }
+    defaults.update(params)
+
+    return Play.objects.create(**defaults)
 
 
 class UnauthenticatedMovieAPITests(TestCase):
@@ -13,3 +28,20 @@ class UnauthenticatedMovieAPITests(TestCase):
     def test_auth_required(self):
         res = self.client.get(PLAY_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedMovieAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test@test.com", password="testpassword"
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_play_list(self):
+        sample_play()
+        res = self.client.get(PLAY_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        plays = Play.objects.all()
+        serializer = PlayListSerializer(plays, many=True)
+        self.assertEqual(res.data["results"], serializer.data)
