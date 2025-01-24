3592c63ec8ca4f44f4e5cdb3dc0e7046d2c4ba9a
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-24 17:43:16 +0200

    fix the bug

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -53,6 +53,18 @@ class PlayListSerializer(PlaySerializer):
         slug_field="full_name"
     )
 
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+            "image",
+        )
+
 
 class PlayDetailSerializer(serializers.ModelSerializer):
     genres = GenreSerializer(many=True, read_only=True)
