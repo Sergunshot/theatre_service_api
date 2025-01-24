d45dad02b57f85861a1ae8503e6fc8bbccaf90c5
Sergiy Yurov <urovsergej10@gmail.com>
2025-01-24 17:02:27 +0200

    add test_image_url_is_shown_on_play_detail

--- theatre/tests/tests_theatre_api.py
+++ theatre/tests/tests_theatre_api.py
@@ -264,7 +264,7 @@ class MovieImageUploadTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_post_image_to_movie_list(self):
+    def test_post_image_to_play_list(self):
         url = PLAY_URL
         with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
             img = Image.new("RGB", (10, 10))
@@ -286,3 +286,14 @@ class MovieImageUploadTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         play = Play.objects.get(title="Title")
         self.assertFalse(play.image)
+
+    def test_image_url_is_shown_on_play_detail(self):
+        url = image_upload_url(self.play.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(detail_url(self.play.id))
+
+        self.assertIn("image", res.data)
